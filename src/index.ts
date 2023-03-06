ffecc94a6b1f147680cab0ce7fe46a9130dc5002
Lucy Poulton <lucy@lucypoulton.net>
2023-03-06 19:06:42 +0000

    multiple projects defined in a config file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {InfluxDB, Point} from "@influxdata/influxdb-client";
 import fastify from "fastify";
 import dotenv from 'dotenv';
 import rateLimit from "@fastify/rate-limit";
+import * as fs from "fs/promises";
+import Project from "./project.js";
 
 dotenv.config()
 
@@ -12,31 +14,14 @@ function notNull<T>(name: string, value: T | null | undefined) {
 
 const PORT = parseInt(notNull('Port', process.env['PORT']))
 const URL = notNull('InfluxDB URL', process.env['INFLUX_URL'])
-const TOKEN =  notNull('InfluxDB Token', process.env['INFLUX_TOKEN'])
-const ORG =  notNull('InfluxDB Org', process.env['INFLUX_ORG'])
-const BUCKET =  notNull('InfluxDB Bucket', process.env['INFLUX_BUCKET'])
-interface ServerInfo {
-    id: string,
-    platform: string,
-    store: string,
-    pluginVersion: string,
-    minecraftVersion: string,
-}
-
-const writeApi = new InfluxDB({url: URL, token: TOKEN}).getWriteApi(ORG, BUCKET, 'ns')
-
-async function write(serverInfo: ServerInfo) {
-    writeApi.writePoint(new Point("servers")
-        .tag("platform", serverInfo.platform)
-        .tag("minecraftVersion", serverInfo.minecraftVersion)
-        .tag("pluginVersion", serverInfo.pluginVersion)
-        .tag("store", serverInfo.store)
-        .stringField("id", serverInfo.id)
-    );
-    await writeApi.flush()
-}
+const TOKEN = notNull('InfluxDB Token', process.env['INFLUX_TOKEN'])
+const influx = new InfluxDB({url: URL, token: TOKEN})
 
 const server = fastify()
+
+const config = JSON.parse(await fs.readFile('config.json', 'utf-8')) as Project[]
+
+console.log(`Loaded ${config.length} projects (${config.map(c => c.name).join(', ')})`)
 
 server.register(rateLimit, {
     max: 60,
@@ -45,33 +30,51 @@ server.register(rateLimit, {
 
 server.get('/', (req, res) => res.status(204).send())
 
-server.post('/v1',
-    {
-        schema: {
-            body: {
-                type: "object",
-                properties: {
-                    id: {type: 'string'},
-                    platform: {type: 'string'},
-                    store: {type: 'string'},
-                    pluginVersion: {type: 'string'},
-                    minecraftVersion: {type: 'string'}
-                },
-                required: ['id', 'platform', 'store', 'pluginVersion', 'minecraftVersion']
-            }
-        }
-    },
-    async (req, res) => {
-        if (!req.headers["user-agent"]?.match(/ProNouns\/.+/)) return res.status(400).send();
-        const body = req.body as ServerInfo
-        try {
-            await write(body)
-            res.status(204).send()
-        } catch (ex) {
-            res.status(500).send()
-        }
-    })
+async function handle(userAgent: string | undefined, body: any): Promise<{ code: number } & ({ error: string } | { point: Point })> {
+    if (userAgent == null) return {code: 400, error: 'Missing user agent'}
+    const project = config.find(p => userAgent.toLowerCase().startsWith(`${p.name.toLowerCase()}/`))
+    if (project == null) return {code: 400, error: 'Invalid user agent'}
+    if (typeof body !== 'object' || body == null) return {code: 422, error: 'Invalid body'}
 
-await server.listen({ port: PORT, host: '0.0.0.0' })
+    const point = new Point()
+    for (const [name, field] of Object.entries(project.fields)) {
+        const value = body[name]
+        if (value == null) {
+            if (field.optional ?? false) continue
+            return {code: 422, error: `Missing required field ${name}`}
+        }
+        if (typeof value !== field.type) return {
+            code: 422,
+            error: `Expected type ${field.type} for ${name} but got ${typeof value}`
+        }
+        if (typeof value == "string") {
+            if (value.length === 0) return {code: 422, error: `${value} must not have a length of 0`}
+            if (value.endsWith(`\\`)) return {code: 422, error: `${value} must not end with a backslash`}
+        }
+        point.tag(name, `${value}`)
+    }
+
+    if (body.id == null) return {code: 422, error: 'Missing required field id'}
+    point.stringField('id', body.id)
+
+    const writeApi = influx.getWriteApi(project.influx.org, project.influx.bucket, 'ns')
+    writeApi.writePoint(point)
+    await writeApi.flush()
+    return {code: 204, point}
+}
+
+server.post('/v1', async (req, res) => {
+    try {
+        const result = await handle(req.headers["user-agent"], req.body)
+        res.status(result.code)
+        if ('error' in result) return res.send(result);
+
+        res.send()
+    } catch (ex) {
+        res.status(500).send()
+    }
+})
+
+await server.listen({port: PORT, host: '0.0.0.0'})
 console.log(`Listening on port ${PORT}`)
 
---
+++ src/project.ts
@@ -0,0 +1,14 @@
+export default interface Project {
+    name: string,
+    fields: {
+        [field: string]: {
+            type: "string" | "number" | "boolean",
+            optional?: boolean
+        }
+    },
+    influx: {
+        org: string,
+        bucket: string
+    }
+}
+
